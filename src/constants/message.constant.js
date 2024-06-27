76b5c5280f8410518cb40b27cd52a77518c3011e
GinGer-JSK <jskim5701@gmail.com>
2024-06-28 02:05:28 +0900

    삭제할거

--- src/constants/message.constant.js
+++ src/constants/message.constant.js
@@ -1,7 +1,26 @@
 export const MESSAGES = {
   AUTH: {
+    COMMON: {
+      EMAIL: {
+        REQUIRED: "email 을 입력해 주세요.",
+        INVALID_FORMAT: "이메일 형식이 올바르지 않습니다.",
+        DUPLICATED: "이미 가입 된 사용자입니다.",
+        EMPTY: "email 은 비어있으면 안됩니다.",
+      },
+      PASSWORD: {
+        REQUIRED: "password 를 입력해 주세요.",
+        MIN_LENGTH: `비밀번호는 6자리 이상이어야 합니다.`,
+      },
+      PASSWORDRE: {
+        REQUIRED: "password 확인을 입력해 주세요.",
+        NOT_MATCHED_WITH_PASSWORD: "입력 한 두 비밀번호가 일치하지 않습니다.",
+      },
+      NAME: {
+        REQUIRED: "name 을 입력해 주세요.",
+      },
+    },
     SIGN_UP: {
-      SUCCEED: "ȸ�����Կ� �����߽��ϴ�",
+      SUCCEED: "회원가입에 성공했습니다",
     },
   },
 };
--- src/middlewares/error-handler.middleware.js
+++ src/middlewares/error-handler.middleware.js
@@ -1,7 +1,8 @@
 export const errorHandler = (err, req, res, next) => {
   console.error(err);
-
-  // joi�뿉�꽌 諛쒖깮�븳 �뿉�윭 泥섎━
+  // console.log(err.name);
+  // console.log(err.name === "ValidationError");
+  // joi에서 발생한 에러 처리
   if (err.name === "ValidationError") {
     return res.status(400).json({
       status: 400,
@@ -9,10 +10,9 @@ export const errorHandler = (err, req, res, next) => {
     });
   }
 
-  // 洹� 諛뽰쓽 �삁�긽移� 紐삵븳 �뿉�윭 泥섎━
+  // 그 밖의 예상치 못한 에러 처리
   return res.status(500).json({
     status: 500,
-    message:
-      "�삁�긽移� 紐삵븳 �뿉�윭媛� 諛쒖깮�뻽�뒿�땲�떎. 愿�由ъ옄�뿉寃� 臾몄쓽�빐 二쇱꽭�슂.",
+    message: "예상치 못한 에러가 발생했습니다. 관리자에게 문의해 주세요.",
   });
 };
